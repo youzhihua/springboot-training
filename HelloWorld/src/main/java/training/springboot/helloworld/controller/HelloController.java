38a786dc116dde4d881113dbdacff882a2fcdaba
youzhihua <1761595830@qq.com>
2019-10-18 22:45:25 +0800

    update hello world demo

---
+++ HelloWorld/src/main/java/training/springboot/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package training.springboot.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello SpringBoot 2.x";
+    }
+
+}
---
+++ HelloWorld/src/test/java/training/springboot/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package training.springboot.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
