213a85d77af427222b2dd6ce4529bbe376626d82
youzhihua <1761595830@qq.com>
2019-11-05 21:11:42 +0800

    update spring boot timer

--- Timer/src/main/java/training/springboot/timer/javatimer/JavaTimer.java
+++ Timer/src/main/java/training/springboot/timer/javatimer/JavaTimer.java
@@ -7,7 +7,7 @@ import java.util.TimerTask;
  * Java timer实现定时器
  */
 public class JavaTimer {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         TimerTask timerTask = new TimerTask() {
             //用于自定义定时器执行的方法
             @Override
@@ -16,12 +16,12 @@ public class JavaTimer {
             }
         };
 
-        Timer timer = new Timer();
-        *//**
+         Timer timer = new Timer();
+         /*
          * timerTask:自定义的TimerTask
          * delay:程序运行后，延时多久启动，单位ms
          * period:每次任务执行间隔，单位ms
-         *//*
+         */
         timer.schedule(timerTask,10000,1000);
-    }*/
+    }
 }
