7b76bb542c14379304f5c44377488d05c47526e4
youzhihua <1761595830@qq.com>
2019-12-04 18:17:04 +0800

    update properties

--- Cache/src/main/java/training/springboot/cache/dao/CustomeDao.java
+++ Cache/src/main/java/training/springboot/cache/dao/CustomeDao.java
@@ -3,5 +3,9 @@ package training.springboot.cache.dao;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+/**
+ * 自定义的mapper模板
+ * @param <T>
+ */
 public interface CustomeDao<T> extends MySqlMapper<T>, Mapper<T> {
 }
--- Cache/src/main/java/training/springboot/cache/service/BookService.java
+++ Cache/src/main/java/training/springboot/cache/service/BookService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import training.springboot.cache.dao.BookDao;
 import training.springboot.cache.pojo.Book;
